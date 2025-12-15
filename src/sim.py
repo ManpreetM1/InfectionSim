bf54ed2aaaa233c43247ae711dcc9bba48968aba
ManpreetM1 <87155707+ManpreetM1@users.noreply.github.com>
2025-12-15 03:10:03 -0700

    Added many changes to the simulation

--- src/sim.py
+++ src/sim.py
@@ -7,47 +7,101 @@ from matplotlib.animation import FuncAnimation
 # -------------------------
 # Simulation Parameters
 # -------------------------
-N_AGENTS = 150
-WIDTH, HEIGHT = 800, 600          # Used only for movement, no pygame needed
-INFECTION_RADIUS = 50
-INFECTION_PROB = 0.9              # Based on the 3 least dense populated states (Alaska, Wyoming, Montana)
-RECOVERY_TIME = 30                # steps until recovery
-STEPS = 300                       # total number of frames
-INITIAL_INFECTION_PROB = 0.025
+N_AGENTS = 250                  # Number of simulated agents
+MAXWIDTH,MAXHEIGHT = 800, 600   # Maximum dimensions of simulation area
+INFECTION_RADIUS = 20           # Radius where infection is possible between susceptible and infected agent
+INFECTION_PROB = 0.9            # Infection probability when in contact, based on the 3 least dense populated states (Alaska, Wyoming, Montana)
+RECOVERY_TIME = 150             # Time steps until recovery
+STEPS = 300                     # Total number of time steps simulated
+SIM_INTERVAL = 5                # The interval at which the simulation generates values
+INITIAL_INFECTION_PROB = 0.025  # Inital probability for an agent to be randomly infected
+
+# Variables based on simulation parameters
+width, height = MAXWIDTH, MAXHEIGHT             # Current Dimensions of simulation area, initalized to maxwidth and maxheight
+vaccine_infection_prob = 0.1 * INFECTION_PROB   # Infection probability under vaccine scenario, approximately 90% reduction
+# Infection probability in shopping centers, approximately 32% increase, it's capped at 1 since greater than 1 probability can't exist
+shopping_infection_prob = max(INFECTION_PROB + (0.32 * INFECTION_PROB), 1) 
+shopping_centres = []
+
+
+class ShoppingCentre:
+    def __init__(self, x=random.uniform(1, width), y=random.uniform(1,height), top_left=[random.uniform(0, width/2), random.uniform(0,height/2)]):
+        self.x = x
+        self.y = y
+        self.top_left = top_left
+        #self.area = (self.x - self.top_left[0]), (self.y - self.top_left[1]) 
+
+    def is_in(self, x, y):
+        # print(f"{self.top_left[0]} < {x} < {self.x + self.top_left[0]} and {self.top_left[1]} < {y} < {self.y + self.top_left[1]}")
+        return(self.top_left[0] < x < self.x + self.top_left[0] and self.top_left[1] < y < self.y + self.top_left[1])
+    
+    def __repr__(self):
+        return f"X:{self.x}, Y:{self.y}, TL: {self.top_left}"
 
 # -------------------------
 # Agent setup
 # -------------------------
 agents = pd.DataFrame({
-    "x": [random.uniform(0, WIDTH) for _ in range(N_AGENTS)],
-    "y": [random.uniform(0, HEIGHT) for _ in range(N_AGENTS)],
+    "x": [random.uniform(0, width) for _ in range(N_AGENTS)],
+    "y": [random.uniform(0, height) for _ in range(N_AGENTS)],
     "vx": [random.uniform(-1.2, 1.2) for _ in range(N_AGENTS)],
     "vy": [random.uniform(-1.2, 1.2) for _ in range(N_AGENTS)],
     "state": ["S"] * N_AGENTS,
     "time_infected": [0] * N_AGENTS,
 })
 
-initial_infection_agents = agents.index.to_series().sample(frac = INITIAL_INFECTION_PROB,replace = False)
+initial_infection_agents = agents.index.to_series().sample(frac = INITIAL_INFECTION_PROB, replace = False)
 infection_mask = agents.index.isin(initial_infection_agents)
 
 # Seed initial infection
 agents.loc[infection_mask, "state"] = "I"
 
+
 sir_log = []   # will store S, I, R values over time
+
+steps_left = SIM_INTERVAL    # Stores the current time steps left before the next work periods starts, initalized at SIM_INTERVAL so it can scale with it
+step_subtraction = (SIM_INTERVAL/3) # Stores the amount that the steps_left variable will be subtracted every loop, can be scaled for more or less work periods
+
+def work_period():
+    global width
+    global height
+    width = MAXWIDTH/2
+    height = MAXHEIGHT/2
+    agents.loc[(agents["x"] > width), "x"] = random.uniform(0, width)
+    agents.loc[(agents["y"] > height), "y"] = random.uniform(0, height)
+
+def generate_shops():
+    shop1 = ShoppingCentre(100, 200, [100, 100])
+    shop2 = ShoppingCentre(200, 100, [200, 200])
+    shopping_centres.append(shop1)
+    shopping_centres.append(shop2)
 
 # -------------------------
 # Simulation step
 # -------------------------
+
+generate_shops()
+
 def update_agents():
     global agents
+    global steps_left
+    global width, height
+    print("max x: " + str(width) + ", max y:" + str(height))
+    print(shopping_centres)
+    if steps_left <= 0:
+        # print(current_step)
+        work_period()
+        steps_left = SIM_INTERVAL
+    else:
+        width, height = MAXWIDTH, MAXHEIGHT
 
     # Move agents
     agents["x"] += agents["vx"]
     agents["y"] += agents["vy"]
 
     # Bounce off walls
-    agents.loc[(agents["x"] < 0) | (agents["x"] > WIDTH), "vx"] *= -1
-    agents.loc[(agents["y"] < 0) | (agents["y"] > HEIGHT), "vy"] *= -1
+    agents.loc[(agents["x"] < 0) | (agents["x"] > width), "vx"] *= -1
+    agents.loc[(agents["y"] < 0) | (agents["y"] > height), "vy"] *= -1
 
     # Infection spread
     infected = agents[agents["state"] == "I"]
@@ -55,12 +109,24 @@ def update_agents():
     for idx, inf in infected.iterrows():
         for jdx, sus in agents[agents["state"] == "S"].iterrows():
             dist = math.hypot(inf["x"] - sus["x"], inf["y"] - sus["y"])
-            if dist < INFECTION_RADIUS and random.random() < INFECTION_PROB:
-                agents.at[jdx, "state"] = "I"
+            if dist < INFECTION_RADIUS:
+                for s in shopping_centres:
+                    if(s.is_in(inf["x"], inf["y"]) and s.is_in(sus["x"], sus["y"])):
+                        infect_risk = shopping_infection_prob
+                    else:
+                        infect_risk = INFECTION_PROB
+                        if random.random() < infect_risk:
+                            agents.at[jdx, "state"] = "I"
+                            # print("Infection at:" + str(agents.at[jdx, "x"]) + ", " + str(agents.at[jdx, "y"]))
+                            # print("Infected at:" + str(sus["x"]) + ", " + str(sus["y"]))
 
     # Update infection timers→recovery
-    agents.loc[agents["state"] == "I", "time_infected"] += 1
+    agents.loc[agents["state"] == "I", "time_infected"] += SIM_INTERVAL
     agents.loc[agents["time_infected"] >= RECOVERY_TIME, "state"] = "R"
+
+    # Updates time steps left unitl work period, set to subtract by a third of the set SIM_INTERVAL 
+    # but can be scaled to change number of work periods in simulation
+    steps_left -= step_subtraction
 
 # -------------------------
 # Matplotlib Bar Graph
@@ -72,9 +138,9 @@ S_vals=[]
 I_vals=[]
 R_vals=[]
 
-line_S, = ax.plot([], [], color="#1100ff", marker='o', label="S")
+line_S, = ax.plot([], [], color="#1100ff", marker='h', label="S")
 line_I, = ax.plot([], [], color="#ff0000", marker='x', label="I")
-line_R, = ax.plot([], [], color="#00ff0d", marker='o', label="R")
+line_R, = ax.plot([], [], color="#00ff0d", marker='d', label="R")
 
 #bars = ax.bar(["S", "I", "R"], [0, 0, 0], color=["#3399ff", "#ff4444", "#44cc88"])
 ax.set_xlim(0, STEPS)
@@ -114,7 +180,7 @@ def animate(frame):
     return line_S, line_I, line_R
 
 # Run animation
-anim = FuncAnimation(fig, animate, frames=range(0, STEPS, 5), interval=300, repeat=False)
+anim = FuncAnimation(fig, animate, frames=range(0, STEPS, SIM_INTERVAL), interval=100, repeat=False)
 plt.show()
 
 # Save SIR results
