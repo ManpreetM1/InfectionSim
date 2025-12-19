932a50102458491174e4a4dfc4e7c7b6fb5cbfe3
ManpreetM1 <87155707+ManpreetM1@users.noreply.github.com>
2025-12-18 21:29:18 -0700

    - Added new parameters

--- src/sim.py
+++ src/sim.py
@@ -5,32 +5,32 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 # Simulation Parameters
-N_AGENTS = 250                  # Number of simulated agents
-MAXWIDTH,MAXHEIGHT = 800, 600   # Maximum dimensions of simulation area
-BASE_INFECTION_RADIUS = 20      # Base radius where infection is possible between susceptible and infected agent
-BASE_INFECTION_PROB = 0.09      # Base infection probability when in contact, based on the 3 least dense populated states (Alaska, Wyoming, Montana)
-RECOVERY_TIME = 100             # Time steps until recovery
-STEPS = 305                     # Total number of time steps simulated
-SIM_INTERVAL = 5                # The interval at which the simulation generates values
-INITIAL_INFECTION_PROB = 0.025  # Inital probability for an agent to be randomly infected
+N_AGENTS = 1400                     # Number of simulated agents (Based off Elk point, Alberta in 2021)
+MAXWIDTH, MAXHEIGHT = 2220, 2220    # Maximum dimensions of simulation area (Based off Elk point, Alberta in 2021)
+BASE_INFECTION_RADIUS = 2          # Base radius where infection is possible between susceptible and infected agent (Quereshi et al. 2020)
+BASE_INFECTION_PROB = 0.011         # Base infection probability when in contact (Ferreti et al. 2024)
+RECOVERY_TIME = 288                 # Hours until recovery (12 days) (Nichita et al. 2022)
+STEPS = 720                         # Total number of time steps simulated in hours, around 30 days
+SIM_INTERVAL = 5                    # The interval at which the simulation generates values, currently set at 5 hours
+INITIAL_INFECTION_PROB = 0.025      # Inital probability for an agent to be randomly infected (Wang et al. 2023)
 
 
 # Variables based on simulation parameters
 width, height = MAXWIDTH, MAXHEIGHT             # Current Dimensions of simulation area, initalized to maxwidth and maxheight
 infection_prob = BASE_INFECTION_PROB            # Current infection probabillty, initalized to base infection probability
 infection_radius = BASE_INFECTION_RADIUS        # Current infection radius, inialized to base infection radius
+agents = None
 
 
 # Intervention variables (Change these to change the simulation)
-vaccination = True
-isolation = True
+vaccination = False
+isolation = False
 
-vaccine_infection_prob = 0.1 * BASE_INFECTION_PROB   # Infection probability under vaccine scenario, approximately 90% reduction (simpleified since vaccine rollout takes time and is not consistent across population)
-isolation_infection_radius = 0.3 * BASE_INFECTION_RADIUS # Infection radius under isolation scenario, approximately 70% reduction
+vaccine_infection_prob = 0.1 * BASE_INFECTION_PROB          # Infection probability under vaccine scenario, approximately 90% reduction (simpleified since vaccine rollout takes time and is not consistent across population)
 
 # Infection probability in shopping centers, approximately 32% increase, it's capped at 1 since greater than 1 probability can't exist
-shopping_infection_prob = min(infection_prob + (0.32 * infection_prob), 1) 
-shopping_centres = []
+shopping_infection_prob = min(infection_prob * 1.32, 1) 
+shopping_centres = []   
 
 
 class ShoppingCentre:
@@ -47,28 +47,17 @@ class ShoppingCentre:
     def __repr__(self):
         return f"X:{self.x}, Y:{self.y}, TL: {self.top_left}"
 
-# Creates the agents using pandas dataframes
-agents = pd.DataFrame({
-    "x": [random.uniform(0, width) for _ in range(N_AGENTS)],
-    "y": [random.uniform(0, height) for _ in range(N_AGENTS)],
-    "vx": [random.uniform(-1.2, 1.2) for _ in range(N_AGENTS)],
-    "vy": [random.uniform(-1.2, 1.2) for _ in range(N_AGENTS)],
-    "state": ["S"] * N_AGENTS,
-    "time_infected": [0] * N_AGENTS,
-})
-
-# Creates a mask from which a subset of agents will be selected
-initial_infection_agents = agents.index.to_series().sample(frac = INITIAL_INFECTION_PROB, replace = False)
-infection_mask = agents.index.isin(initial_infection_agents)
-
-# Seed initial infection in those chosen agents from the previous mask created
-agents.loc[infection_mask, "state"] = "I"
-
-
-sir_log = []                 # will store S, I, R values over time
-
-steps_left = SIM_INTERVAL    # Stores the current time steps left before the next work periods starts, initalized at SIM_INTERVAL so it can scale with it
-step_subtraction = (SIM_INTERVAL/3) # Stores the amount that the steps_left variable will be subtracted every loop, can be scaled for more or less work periods
+def create_agents(n):
+    global agents
+    # Creates the agents using pandas dataframes
+    agents = pd.DataFrame({
+        "x": [random.uniform(0, width) for _ in range(N_AGENTS)],
+        "y": [random.uniform(0, height) for _ in range(N_AGENTS)],
+        "vx": [random.uniform(-1.2, 1.2) for _ in range(N_AGENTS)],
+        "vy": [random.uniform(-1.2, 1.2) for _ in range(N_AGENTS)],
+        "state": ["S"] * N_AGENTS,
+        "time_infected": [0] * N_AGENTS,
+    })
 
 # Simulates the working period by forcing the agents into a smaller boundary
 def work_period():
@@ -89,13 +78,24 @@ def generate_shops():
 
 
 # Simulation starts here
+sir_log = []                 # will store S, I, R values over time
+
+steps_left = SIM_INTERVAL    # Stores the current time steps left before the next work periods starts, initalized at SIM_INTERVAL so it can scale with it
+step_subtraction = (SIM_INTERVAL/3) # Stores the amount that the steps_left variable will be subtracted every loop, can be scaled for more or less work periods
 
 generate_shops()
 
 if(vaccination): 
     infection_prob = vaccine_infection_prob
-if(isolation):
-    infection_radius = isolation_infection_radius
+
+create_agents(N_AGENTS)
+
+# Creates a mask from which a subset of agents will be selected
+initial_infection_agents = agents.index.to_series().sample(frac = INITIAL_INFECTION_PROB, replace = False)
+infection_mask = agents.index.isin(initial_infection_agents)
+
+# Seed initial infection in those chosen agents from the previous mask created
+agents.loc[infection_mask, "state"] = "I"
 
 def update_agents():
     global agents
@@ -145,9 +145,16 @@ def update_agents():
     # Update infection timer
     agents.loc[agents["state"] == "I", "time_infected"] += SIM_INTERVAL
 
+    # 9% of infected population is quarantined, considered recovered for our purposes under isolation scenario(Aleta et al. 2020)
+    if(isolation):                              
+        infected_agents = agents.loc[(agents["state"] == "I")]  
+        quarantined_agents = infected_agents.sample(frac = 0.09, replace = False)   
+        quarantine_mask = agents.index.isin(quarantined_agents.index)
+        agents.loc[quarantine_mask, "state"] = "R"
+    
     # A random sampling of infected agents
-    infected_agents = agents.loc[(agents["state"] == "I") & (agents["time_infected"] >= RECOVERY_TIME)]
-    recovery_agents = infected_agents.sample(frac = 0.1, replace = False)
+    infected_eligible_agents = agents.loc[(agents["state"] == "I") & (agents["time_infected"] >= RECOVERY_TIME)]
+    recovery_agents = infected_eligible_agents.sample(frac = 0.1, replace = False)  # 10% of agents past recovery time will be recovered every day to simulate differences in immunity
 
     #print(len(infected_agents))
     #print(len(recovery_agents))
